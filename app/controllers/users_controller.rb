3969042a8dfa303902908834fb350e0942e71d09
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-03-29 14:49:01 -0300

    Adding erros info alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+    end
+
     def new
         @user = User.new
     end
@@ -14,7 +19,7 @@ class UsersController < ApplicationController
     end
 
     private
-    
+
     def user_params
         params.
         require(:user).
