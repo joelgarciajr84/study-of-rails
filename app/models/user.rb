cb467cf178a202477715c2e8a3ad4a33546640b2
Joel Garcia Jr <joel.garciajr84@gmail.com>
2016-03-30 21:52:13 -0300

    New features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,9 @@
 class User < ActiveRecord::Base
-    
     EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-    validates_presence_of :email, :full_name, :location, :password
-    validates_confirmation_of :password
+
+    validates_presence_of :email, :full_name, :location
     validates_length_of :bio, minimum: 30, allow_blank: false
-    validates_uniqueness_of :email
-    
-    # Essa validação pode ser representada da seguinte forma:
-    # validates_format_of :email, with: EMAIL_REGEXP
-    
-    validate do
-        errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-    end
+    validates_format_of :email, with: EMAIL_REGEXP
+
+    has_secure_password
 end
